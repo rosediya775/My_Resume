dde0191c582c2a53c9b44605b48f308cc2faed14
diyarose775 <diyarose1988@gmail.com>
2022-07-22 14:37:38 -0400

    First commit

---
+++ My Resume/src/rPackage/DataTypes.java
@@ -0,0 +1,79 @@
+package rPackage;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("\t\t\tResume");
+System.out.println("\nPersonal Qualification");
+System.out.println("\nName");
+System.out.println("Sadia Afroz");
+System.out.println("Age");
+byte b = 33;
+System.out.println(b);
+System.out.println("Phone");
+long l = 9178914633l;
+System.out.println(l);
+System.out.println("Email");
+System.out.println("diyarose1988@gmail.com");
+System.out.println("\n\nEducation");
+System.out.println("\nMasters of Arts in English");
+System.out.println("Stamford University Bangladesh");
+System.out.println("Dhaka Bangladesh");
+int a = 2015;
+System.out.println(a);
+System.out.println("\nBachelor of Arts with Honours in English");
+System.out.println("Stamford University Bangladesh");
+System.out.println("Dhaka Bangladesh");
+int a2 = 2011;
+System.out.println(a2);
+System.out.println("\n\nExperience");
+System.out.println("\nGrader");
+System.out.println("2018-2019");
+System.out.println("GSM Club LLC");
+System.out.println("1118 Longwood ave, Bronx, NY, 10474");
+System.out.println("\n\nCertificaton");
+System.out.println("\nAssessment of Teaching Assistant skill (ATAS)(095)");
+System.out.println("\n\nSkills");
+System.out.println("\nPatience");
+System.out.println("Communication");
+System.out.println("Organization");
+System.out.println("Teamwork and Collaboration");
+System.out.println("Interpersonal skills");
+System.out.println("Time Management");
+System.out.println("Customer Service");
+System.out.println("\n\nInterest");
+System.out.println("\nArt");
+System.out.println("Listening to Music");
+System.out.println("Gardening");
+System.out.println("Singing");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
